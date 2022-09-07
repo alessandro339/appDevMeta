51b045681cf46e0f2aed7594ef04aad3014d1fce
Silva <ASilva06@lear.com>
2022-09-06 21:24:53 -0300

    Project created

---
+++ backend/src/test/java/com/appAlessandro/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appAlessandro.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
